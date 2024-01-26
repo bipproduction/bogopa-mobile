8c19fa8e6718c2a232e5140a4e85d9d3265098c2
lukman <lukman@lukmans-MacBook-Air.local>
2024-01-26 15:16:22 +0800

    style : update live
    Deskripsi:
    - update live page
    
    No Issue

--- lib/presentation/live/page/view_live_page.dart
+++ lib/presentation/live/page/view_live_page.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../../core/assets/assets.gen.dart';
 import '../../../core/constants/colors.dart';
+import '../witget/rekomendasi_page.dart';
+import '../witget/sekitar_page.dart';
 
 class ViewLivePage extends StatelessWidget {
   const ViewLivePage({super.key});
@@ -11,54 +13,54 @@ class ViewLivePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: AppColors.putih,
-        appBar: AppBar(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
           backgroundColor: AppColors.putih,
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {},
-              child: Assets.icons.live.svg(
-                  height: 24,
-                  width: 24,
-                  // ignore: deprecated_member_use_from_same_package
-                  color: AppColors.coklat),
-            )
-          ],
-          title: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'LIVE BOGOPA',
-                softWrap: false,
-                overflow: TextOverflow.fade,
-                style: GoogleFonts.poppins(
-                    textStyle: const TextStyle(
-                        fontSize: 26.0,
-                        color: AppColors.coklat,
-                        fontWeight: FontWeight.bold)),
-              ),
+          appBar: AppBar(
+            backgroundColor: AppColors.putih,
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {},
+                child: Assets.icons.live.svg(
+                    height: 24,
+                    width: 24,
+                    // ignore: deprecated_member_use_from_same_package
+                    color: AppColors.coklat),
+              )
             ],
+            title: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'LIVE BOGOPA',
+                  softWrap: false,
+                  overflow: TextOverflow.fade,
+                  style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          fontSize: 26.0,
+                          color: AppColors.coklat,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ],
+            ),
+            bottom:  const TabBar(
+              indicatorColor: AppColors.pinkMerah,
+              labelColor: AppColors.pinkMerah,
+              unselectedLabelColor: AppColors.coklat,
+              dividerColor: Colors.transparent,
+              tabs: [
+                Tab(text: 'Rekomendasi'),
+                Tab(text: 'Sekitar'),
+              ],
+            ),
           ),
-        ),
-        body: const SingleChildScrollView(
-          child: LivePage(),
+          body: const TabBarView(children: [
+            RekomendasiPage(),
+            SekitarPage(),
+          ]),
         ),
       ),
     );
-  }
-}
-
-class LivePage extends StatefulWidget {
-  const LivePage({super.key});
-
-  @override
-  State<LivePage> createState() => _LivePageState();
-}
-
-class _LivePageState extends State<LivePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
---
+++ lib/presentation/live/witget/rekomendasi_page.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../core/constants/colors.dart';
+
+class RekomendasiPage extends StatefulWidget {
+  const RekomendasiPage({super.key});
+
+  @override
+  State<RekomendasiPage> createState() => _RekomendasiPageState();
+}
+
+class _RekomendasiPageState extends State<RekomendasiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Expanded(
+          child: Column(
+        children: <Widget>[
+          Expanded(
+            child: GridView.count(
+              primary: false,
+              crossAxisCount: 2,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Stack(
+                    alignment: AlignmentDirectional.bottomEnd,
+                    fit: StackFit.passthrough,
+                    children: <Widget>[
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: AppColors.pink,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: InkWell(
+                            onTap: () {},
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    'https://i.pravatar.cc/200?img=20')),
+                          )),
+                      Container(
+                        decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10)),
+                              gradient: LinearGradient(
+                                  begin: FractionalOffset.topCenter,
+                                  end: FractionalOffset.bottomCenter,
+                                  colors: [
+                                    Colors.white.withOpacity(0.0),
+                                    Colors.black,
+                                  ],
+                                  stops: const [
+                                    0.0,
+                                    1.0,
+                                  ])),
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      'Selamat Datang',
+                                      softWrap: false,
+                                      overflow: TextOverflow.fade,
+                                      style: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontSize: 11.0,
+                                              color: AppColors.putih,
+                                              fontWeight: FontWeight.normal)),
+                                    ),
+                                    Text(
+                                      'Jakarta Barat',
+                                      softWrap: false,
+                                      overflow: TextOverflow.fade,
+                                      style: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontSize: 11.0,
+                                              color: AppColors.putih,
+                                              fontWeight: FontWeight.normal)),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(0),
+                                  child: Text(
+                                    '21',
+                                    softWrap: false,
+                                    overflow: TextOverflow.fade,
+                                    style: GoogleFonts.poppins(
+                                        textStyle: const TextStyle(
+                                            fontSize: 13.0,
+                                            color: AppColors.putih,
+                                            fontWeight: FontWeight.normal)),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Stack(
+                    alignment: AlignmentDirectional.bottomEnd,
+                    fit: StackFit.passthrough,
+                    children: <Widget>[
+                      // ignore: avoid_unnecessary_containers
+                      Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: AppColors.pink,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: InkWell(
+                            onTap: () {},
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    'https://i.pravatar.cc/200?img=21')),
+                          )),
+                      Container(
+                        decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10)),
+                              gradient: LinearGradient(
+                                  begin: FractionalOffset.topCenter,
+                                  end: FractionalOffset.bottomCenter,
+                                  colors: [
+                                    Colors.white.withOpacity(0.0),
+                                    Colors.black,
+                                  ],
+                                  stops: const [
+                                    0.0,
+                                    1.0,
+                                  ])),
+                          alignment: Alignment.bottomRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      'Gass Polll',
+                                      softWrap: false,
+                                      overflow: TextOverflow.fade,
+                                      style: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontSize: 11.0,
+                                              color: AppColors.putih,
+                                              fontWeight: FontWeight.normal)),
+                                    ),
+                                    Text(
+                                      'Jakarta Timur',
+                                      softWrap: false,
+                                      overflow: TextOverflow.fade,
+                                      style: GoogleFonts.poppins(
+                                          textStyle: const TextStyle(
+                                              fontSize: 11.0,
+                                              color: AppColors.putih,
+                                              fontWeight: FontWeight.normal)),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(0),
+                                  child: Text(
+                                    '21',
+                                    softWrap: false,
+                                    overflow: TextOverflow.fade,
+                                    style: GoogleFonts.poppins(
+                                        textStyle: const TextStyle(
+                                            fontSize: 13.0,
+                                            color: AppColors.putih,
+                                            fontWeight: FontWeight.normal)),
+                                  ),
+                                )
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
