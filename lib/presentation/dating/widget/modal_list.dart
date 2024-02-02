035c173064792728483a52031170100e9ea5c400
lukman <lukman@lukmans-MacBook-Air.local>
2024-02-02 17:46:39 +0800

    style : update lib
    Deskripsi:
    - update langganan
    - update dialog vip
    
    No Issue

--- lib/presentation/dating/widget/modal_list.dart
+++ lib/presentation/dating/widget/modal_list.dart
@@ -1,8 +1,11 @@
+import 'package:bogopa_mobile/presentation/profile/widget/card1/harga_langganan_1.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../core/assets/assets.gen.dart';
 import '../../../core/constants/colors.dart';
+import 'button_dialog_putar_bali.dart';
+import 'dialog_putar_balik.dart';
 
 class ModalList extends StatelessWidget {
   const ModalList({super.key});
@@ -22,75 +25,96 @@ class ViewModalList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: const Badge(
-          label: Text(
-            'VIP',
-            style: TextStyle(color: AppColors.coklat),
-          ),
-          backgroundColor: Colors.yellow,
-          child: Icon(
-            Icons.settings_backup_restore,
-            color: AppColors.coklat,
-          )),
-      onPressed: () => _dialogBuilder(context),
+        icon: const Badge(
+            label: Text(
+              'VIP',
+              style: TextStyle(color: AppColors.coklat),
+            ),
+            backgroundColor: Colors.yellow,
+            child: Icon(
+              Icons.settings_backup_restore,
+              color: AppColors.coklat,
+            )),
+        onPressed: () {
+          showDialog(
+              context: context, builder: (context) => const CustomDialogData());
+        });
+  }
+}
+
+class CustomDialogData extends StatelessWidget {
+  const CustomDialogData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Stack(
+        children: [
+          const CardDialog(),
+          Positioned(
+            height: 30,
+            width: 30,
+            child: OutlinedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Icon(Icons.cancel)
+            ),
+          )
+        ],
+      ),
     );
   }
+}
 
-  Future<void> _dialogBuilder(BuildContext context) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: AppColors.putih,
-          title: Padding(
-              padding: const EdgeInsets.only(top: 0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.end, 
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Icon(
-                          Icons.close,
-                          color: AppColors.coklat,
-                          size: 20.0,
-                        ),
-                      )
-                    ]),
-                    Text(
-                      'UMPAN BALIK',
-                      softWrap: false,
-                      overflow: TextOverflow.fade,
-                      style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                              fontSize: 20.0,
-                              color: AppColors.pinkMerah,
-                              fontWeight: FontWeight.bold)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipOval(
-                        child: SizedBox.fromSize(
-                          size: const Size.fromRadius(56.0),
-                          child: Assets.images.fotoUser.image(
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    )
-                  ])),
-          content: const Text(
-            'A dialog is a type of modal window that\n'
-            'appears in front of app content to\n'
-            'provide critical information, or prompt\n'
-            'for a decision to be made.',
+class CardDialog extends StatelessWidget {
+  const CardDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 436,
+      decoration: BoxDecoration(
+          color: AppColors.putih, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(
+            height: 20,
           ),
-        );
-      },
+          Text(
+            'PUTAR BALIK',
+            softWrap: false,
+            overflow: TextOverflow.fade,
+            style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                    fontSize: 20.0,
+                    color: AppColors.pinkMerah,
+                    fontWeight: FontWeight.bold)),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const CircleAvatar(
+            radius: 44,
+            backgroundColor: AppColors.pink,
+            child: CircleAvatar(
+                radius: 40,
+                backgroundImage:
+                    NetworkImage('https://i.pravatar.cc/200?img=4')),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const DialogPutarBalik(),
+          const SizedBox(
+            height: 20,
+          ),
+          ButtonDialogPutarBalik()
+        ],
+      ),
     );
   }
 }
